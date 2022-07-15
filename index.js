0ce5f4888f46fca36634b55e5e41b8e6de68a32f
Imran Nur Hirey <95400512+imranhirey@users.noreply.github.com>
2022-07-15 05:21:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let express=require('express')
 let app= express()
 app.get('/',(req,res)=>{
-    res.send('helo bro soo dhawaaw')
+    res.send('<h1>CK AAD BUU U AQLAAQ DARANYAHAY<h1>')
 })
 
 app.listen(8000)
